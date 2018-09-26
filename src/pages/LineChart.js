0aa265b14f2453ee9d8d046ea8b4ac3f809711ac
Mikyxello <anselmi.1716741@studenti.uniroma1.it>
2018-09-26 19:13:09 +0200

    Fixed line charts, added features with flags and names

--- src/pages/LineChart.js
+++ src/pages/LineChart.js
@@ -5,9 +5,9 @@ import Layout from '../components/Layout';
 import classnames from 'classnames';
 import {Line} from 'react-chartjs-2';
 import queryString from 'query-string';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import LastFMLogo from '../lastfm.png';
-import { Link } from 'react-router-dom';
-
+import Flag from "react-flags";
 
 const styles = {
   link: {
@@ -17,23 +17,76 @@ const styles = {
     'border': '1px solid #C0C0C0',
     'border-radius': '15px 50px 30px',
     'padding': '5px',
+    margin: 10
   },
+  root: {
+    flexGrow: 1
+  }
 };
 
 class LineChart extends Component {
   state = {
     err: null,
     loaded: false,
-    data: null,
     labels: null,
-    datasets: null,
+    country: null,
+    countrycode: null,
+    countrycodelong: null,
+    datasets: null
   };
 
   componentDidMount() {
     let params = queryString.parse(this.props.location.search);
     var country = params.country ? params.country : 'all';
-    var url = 'https://cors-anywhere.herokuapp.com/https://rss.itunes.apple.com/api/v1/' + country + '/itunes-music/top-songs/all/50/explicit.json';
+  
+    var itunesUrl = 'https://cors-anywhere.herokuapp.com/https://rss.itunes.apple.com/api/v1/' + country + '/itunes-music/top-songs/all/50/explicit.json';
+    
+    var countrycode;
+    switch (country) {
+      case 'SP':
+        country = 'Spain';
+        countrycode = 'ESP';
+        break;
+      case 'US':
+        country = 'United States';
+        countrycode = 'USA';
+        break;
+      case 'FR':
+        country = 'France';
+        countrycode = 'FRA';
+        break;
+      case 'UK':
+        country = 'United Kingdom';
+        countrycode = 'GBR';
+        break;
+      case 'IT':
+        country = 'Italy';
+        countrycode = 'ITA';
+        break;
+      case 'GR':
+        country = 'Greece';
+        countrycode = 'GRC';
+        break;
+      case 'RS':
+        country = 'Russia';
+        countrycode = 'RUS';
+        break;
+      case 'PL':
+        country = 'Poland';
+        countrycode = 'POL';
+        break;
+      case 'SA':
+        country = 'Saudi Arabia';
+        countrycode = 'SAU';
+        break;
+      default:
+        country = 'World';
+        countrycode = "   ";
+        break;
+    };
 
+    this.setState({ country: country, countrycode: countrycode });
+    
     var labels = [];
     var datasets = [
       {
@@ -41,79 +94,47 @@ class LineChart extends Component {
         pointBackgroundColor: "#36A2EB",
         borderColor: "#36A2EB",
         pointStyle: [],
-        data: [],
+        data: []
       },
       {
         label: "# Plays",
         pointBackgroundColor: "#FE767B",
         borderColor: "#FE767B",
         pointStyle: [],
-        data: [],
+        data: []
       }
     ];
 
-    fetch(url)
+    fetch(itunesUrl)
       .then(response => response.json())
-      .then(data => {
-        switch (params.country) {
-          case 'SP':
-            country = 'Spain';
-            break;
-          case 'US':
-            country = 'United States';
-            break;
-          case 'FR':
-            country = 'France';
-            break;
-          case 'UK':
-            country = 'United Kingdom';
-            break;
-          case 'IT':
-            country = 'Italy';
-            break;
-          case 'GR':
-            country = 'Greece';
-            break;
-          case 'RS':
-            country = 'Russia';
-            break;
-          case 'PL':
-            country = 'Poland';
-            break;
-          case 'SA':
-            country = 'Saudi Arabia';
-            break;
-          default:
-            country = 'Italy';
-            break;
-        };
-
-        for (let item of data.feed.results) {  
-          var itemUrl = 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=5a932a24e44f718c2542eac0fb48309a&artist=' + item.artistName + '&track=' + item.name + '&format=json';
+      .then(itunes => {
+        for (let item of itunes.feed.results) {  
+          var lastfmUrl = 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=5a932a24e44f718c2542eac0fb48309a&artist=' + item.artistName + '&track=' + item.name + '&format=json';
           
-          fetch(itemUrl)
+          fetch(lastfmUrl)
           .then(response => response.json())
-          .then(data => {
-              if ('error' in data){
-                //console.log("TRACK NOT FOUND: " + item.artistName + " — " + item.name)
+          .then(lastfm => {
+              if ('error' in lastfm){
+                console.error("LastFM Error: " + item.artistName + " — " + item.name + " not found");
               }
               else{
                 labels.push(item.artistName + " — " + item.name);
-
-                datasets[0].data.push(data.track.listeners);
-                datasets[1].data.push(data.track.playcount);
+                datasets[0].data.push(lastfm.track.listeners);
+                datasets[1].data.push(lastfm.track.playcount);
               }
+          }).then(() => {
+            if(itunes.feed.results[itunes.feed.results.length-1] === item) {
+              this.setState({ labels: labels, datasets: datasets, loaded: true });
             }
-          );
+          });
         }
-
-        this.setState({ labels: labels, datasets: datasets, loaded: true });
-    });
+      }
+    );
   }
 
   render() {
     const { classes } = this.props;
-    var { loaded, err, labels, datasets } = this.state;
+    var { loaded, err, labels, datasets, country, countrycode } = this.state;
     
     if (err){
       return(
@@ -123,9 +144,14 @@ class LineChart extends Component {
       );
     }
     if (!loaded) {
+      const loading = 
+      <div className={classes.root}>
+        <LinearProgress color="secondary" />
+      </div>
+
       return(
         <div className="App">
-          <Layout yield='Loading Chart...' ></Layout> 
+          <Layout yield={loading} ></Layout> 
         </div>
       );    
     }
@@ -133,19 +159,19 @@ class LineChart extends Component {
     else{
       var chartData = {
         labels: labels,
-        datasets: datasets,
+        datasets: datasets
       };
 
       const chartOptions = {
         elements: {
           line: {
-              tension: 0,
-          },
+              tension: 0
+          }
         },
         scales: {
           yAxes: [{
             ticks: {
-              min: 0,
+              min: 0
             },
           }],
           xAxes: [{
@@ -153,17 +179,25 @@ class LineChart extends Component {
             ticks: {
               autoSkip: false,
               maxRotation: 75,
-              minRotation: 75,
-            },
+              minRotation: 75
+            }
           }]
-        },
+        }
       };
 
       const renderChart = 
       <div className={classnames.root}>
-        <Link to='https://www.last.fm/' className={classes.link} >
-        <span>{"Made with "}<img className={classes.linkImage} src={LastFMLogo} alt="LastFM" width="50px" /></span>
-        </Link>
+        <Flag
+          basePath="/img/flags"
+          name={countrycode}
+          format="png"
+          pngSize={16}
+          shiny={true}
+          alt={country + " Flag "}
+        />{country + " — "}
+        <a href="https://www.last.fm/" className={classes.link} >
+          <span>{"Made with "}<img className={classes.linkImage} src={LastFMLogo} alt="LastFM" width="50px" /></span>
+        </a>
         <Line data={chartData} options={chartOptions} />
       </div>
       return (
